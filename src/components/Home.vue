b1bc2d50047e00d0785442e0a0e4a423b1b8146a
ctola91 <ctolapacheco@gmail.com>
2022-02-23 09:58:19 -0400

    add Recipe list implementation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,14 +11,52 @@ import { DefineComponent, defineComponent } from 'vue';
       <div class="title-container">
         <h1 class="title">What are we cooking?</h1>
       </div>
-      <div>There are no recipes assigned</div>
+      <div v-if="recipes.length > 0">
+        <ul>
+          <li v-for="recipe in recipes" @key="recipe.id">
+            <a href="#">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://bulma.io/images/placeholders/1280x960.png"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">{{ recipe.title }}</p>
+                      <p class="subtitle is-6">{{ recipe.description }}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div v-else>There are no recipes assigned</div>
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
+import { ref, Ref, defineComponent, computed, onMounted } from "vue";
+import RecipeService from "../services/RecipeService";
+import { Recipe } from "../types/RecipeType";
+
 export default defineComponent({
   setup() {
-    return {};
+    const recipes: Ref<Recipe[]> = ref([]);
+
+    onMounted(async () => {
+      recipes.value = await RecipeService.getRecipes();
+    });
+
+    return {
+      recipes,
+    };
   },
 });
 </script>
--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -23,12 +23,20 @@ const addNewRecipe = async ({ title, description, ingredients, instructions, ima
 const getRecipes = async () => {
     try {
         const querySnapshot = await getDocs(collection(db, 'recipes'));
-        let result : Array<Recipe> = [];
+        let result: Array<Recipe> = [];
         querySnapshot.forEach(doc => {
-            console.log(`${doc.id} => ${doc.data()}`);
-            // let recipeDoc = doc.data();
-            // result.push(recipeDoc);
+            let recipeDoc = doc.data();
+            console.table(recipeDoc.title);
+            result.push({
+                id: <string>doc.id,
+                title: <string>recipeDoc.title,
+                description: <string>recipeDoc.description,
+                ingredients: <Array<string>>recipeDoc.ingredients,
+                instructions: <string>recipeDoc.instructions,
+                images: []
+            });
         });
+        console.log(result);
         return result;
     } catch (e) {
         console.error('Error reading documents');
